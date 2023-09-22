0d8793dbbc2219b7a579eb627b5a3d9bbb6d1e27
Marta Kobylinska <68684351+mmkobylin@users.noreply.github.com>
2023-09-22 21:46:39 +0100

    07-total-created

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,11 +3,17 @@ import { Input } from './Input'
 import { Item } from './Item'
 import { v4 as uuidv4 } from 'uuid';
 import { FirstRow } from './FirstRow';
+import Total from './Total';
 
 export const ShoppingList = () => {
 
     //array to store information 
     const [ list, setList ] = useState([])
+
+    // adding the total from the list 
+    // ok so you don't work cause you are not a primitive type. 
+    // what I can do is perhaps use a function to pass it which will return the total anyway? 
+    //const [ total, setTotal ] = useState([0])
 
     // add item with a random id 
     const addItem = item => {
@@ -25,6 +31,14 @@ export const ShoppingList = () => {
             item => item.id === id ? { ...item, purchased: !item.purchased } : item ) ) 
     }
 
+    // do we even need a state? maybe I can pass it alone? 
+    //array.reduce basically reduces it down to one single value 
+
+    //reduce takes two values, one is the function and second one is the starting point - here 0 
+    const totalPrice = list.reduce( ( total, item ) => { 
+        return total + item.price
+    }, 0 )
+
   return (
     <>
         <Input addItem = { addItem } />
@@ -36,6 +50,8 @@ export const ShoppingList = () => {
                 crossItem = { crossItem }
                 />
             )}
+        {/* we can do it with props I suppose */}
+        <Total total = { totalPrice } /> 
     </>
   )
 }
